67cb28e800d8dd930edac3db17ae722ed5eb8736
Jung jiyong <jjy9331@naver.com>
2023-05-12 22:29:37 +0900

    header_style_done1

--- pfjjy/pages/_document.js
+++ pfjjy/pages/_document.js
@@ -1,10 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Header from '../pages/header.js'
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <body>
+        <Header></Header>
         <Main />
         <NextScript />
       </body>
---
+++ pfjjy/pages/header.js
@@ -0,0 +1,43 @@
+
+
+export default function Header() {
+    return (
+        <div className="header">
+            <div className="inner">
+                <div>
+                    <a href="#" class="h_btn" data-value="0">
+                            <img src="/img/logo.png" alt="logo" class="logo h_btn" data-value="0" loading="lazy"/>
+                    </a>
+                </div>
+                <ul>
+                <li>
+                    <a href="#home" class="h_btn" data-value="0">홈</a>
+                    <div class="hr_d1"></div>
+                </li>
+                <li>
+                    <a href="#introduce" class="h_btn" data-value="14775">자기소개</a>
+                    <div class="hr_d2"></div>
+                    <div class="hr_d3"></div>
+                    <div class="hr_d4"></div>
+                    <div class="hr_d5"></div>
+                </li>
+                <li>
+                    <a href="#portfolio" class="h_btn" data-value="63996">포트폴리오</a>
+                    <div class="hr_d6"></div>
+                    <div class="hr_d7"></div>
+                    <div class="hr_d8"></div>
+                    <div class="hr_d9"></div>
+                    <div class="hr_d10"></div>
+                </li>
+                <li>
+                    <a href="#contact" class="h_btn" data-value="218740">컨텍트</a>
+                    <div class="hr_d11"></div>
+                    <div class="hr_d12"></div>
+                    <div class="hr_d13"></div>
+                    <div class="hr_d14"></div>
+                </li>
+            </ul>
+            </div>
+        </div>
+    )
+}
