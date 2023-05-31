6450f65c56107dc9ee59c7cbe4da8bbef510526d
jjy331 <kingjjy31@gmail.com>
2023-05-31 17:44:27 +0900

    html elm console checker2

--- pfjjy/pages/cursor.js
+++ pfjjy/pages/cursor.js
@@ -26,6 +26,7 @@ export default function CustomCursor() {
 
         return () => {
             removeEventListeners();
+            document.removeEventListener('click', handleClick);
         }
     }, []);
 
