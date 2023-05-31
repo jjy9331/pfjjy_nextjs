364da833915b15d83344cc68b9735ea8953bbe10
jjy9331 <kingjjy31@gmail.com>
2023-05-31 19:18:19 +0900

    pf_scroll_guide_title add

--- pfjjy/pages/index.js
+++ pfjjy/pages/index.js
@@ -1,9 +1,11 @@
 import CustomCursor from '../pages/cursor.js'
+import PL_SD from '../pages/pl_sd.js'
 
 export default function Home() {
   return (
     <div>
       <CustomCursor />
+      <PL_SD />
     </div>
   )
 }
