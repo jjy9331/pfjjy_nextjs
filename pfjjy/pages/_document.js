2ed48b18dc11892be0505b12c4c2d0189c442b4a
jjy331 <kingjjy31@gamil.com>
2023-06-01 13:37:49 +0900

    hfm_fuction toggle & escape done

--- pfjjy/pages/_document.js
+++ pfjjy/pages/_document.js
@@ -1,19 +1,12 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Header from '../pages/header.js'
-import Footer from '../pages/footer.js'
-import Hfm from '../pages/hfm.js'
-import CustomCursor from '../pages/cursor.js'
 
 export default function Document() {
   return (
     <Html lang="ko">
       <Head />
       <body>
-        <Header />
         <Main />
         <NextScript />
-        <Hfm />
-        <Footer />
       </body>
     </Html>
   )
--- pfjjy/pages/header.js
+++ pfjjy/pages/header.js
@@ -5,36 +5,36 @@ export default function Header() {
         <div className="header">
             <div className="inner">
                 <div>
-                    <a href="#" class="h_btn" data-value="0">
-                            <img src="/img/logo.png" alt="logo" class="logo h_btn" data-value="0" loading="lazy"/>
+                    <a href="#" className="h_btn" data-value="0">
+                            <img src="/img/logo.png" alt="logo" className="logo h_btn" data-value="0" loading="lazy"/>
                     </a>
                 </div>
                 <ul>
                 <li>
-                    <a href="#home" class="h_btn" data-value="0">홈</a>
-                    <div class="hr_d1"></div>
+                    <a href="#home" className="h_btn" data-value="0">홈</a>
+                    <div className="hr_d1"></div>
                 </li>
                 <li>
-                    <a href="#introduce" class="h_btn" data-value="14775">자기소개</a>
-                    <div class="hr_d2"></div>
-                    <div class="hr_d3"></div>
-                    <div class="hr_d4"></div>
-                    <div class="hr_d5"></div>
+                    <a href="#introduce" className="h_btn" data-value="14775">자기소개</a>
+                    <div className="hr_d2"></div>
+                    <div className="hr_d3"></div>
+                    <div className="hr_d4"></div>
+                    <div className="hr_d5"></div>
                 </li>
                 <li>
-                    <a href="#portfolio" class="h_btn" data-value="63996">포트폴리오</a>
-                    <div class="hr_d6"></div>
-                    <div class="hr_d7"></div>
-                    <div class="hr_d8"></div>
-                    <div class="hr_d9"></div>
-                    <div class="hr_d10"></div>
+                    <a href="#portfolio" className="h_btn" data-value="63996">포트폴리오</a>
+                    <div className="hr_d6"></div>
+                    <div className="hr_d7"></div>
+                    <div className="hr_d8"></div>
+                    <div className="hr_d9"></div>
+                    <div className="hr_d10"></div>
                 </li>
                 <li>
-                    <a href="#contact" class="h_btn" data-value="218740">컨텍트</a>
-                    <div class="hr_d11"></div>
-                    <div class="hr_d12"></div>
-                    <div class="hr_d13"></div>
-                    <div class="hr_d14"></div>
+                    <a href="#contact" className="h_btn" data-value="218740">컨텍트</a>
+                    <div className="hr_d11"></div>
+                    <div className="hr_d12"></div>
+                    <div className="hr_d13"></div>
+                    <div className="hr_d14"></div>
                 </li>
             </ul>
             </div>
