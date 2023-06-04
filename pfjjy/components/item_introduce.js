87d6de7757b17cd3ed99507b0f3193c9f30aec7d
jjy331 <kingjjy31@gmail.com>
2023-06-04 18:37:18 +0900

    next.js sq ani issue keeping

--- pfjjy/components/item_introduce.js
+++ pfjjy/components/item_introduce.js
@@ -1,6 +1,9 @@
 import  React, {useRef, useEffect} from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Lottie from "react-lottie-player";
+import lottie_intro from "../public/data/introduce_ani.json";
+// import { LottieInteractive } from "lottie-interactive";
 
 
 export default function Item_introduce() {
--- pfjjy/pages/index.js
+++ pfjjy/pages/index.js
@@ -4,8 +4,12 @@ import Footer from '../components/footer.js'
 import Item_home from '../components/item_home.js'
 import Sctracker from '../components/sctracker.js'
 import Item_introduce from '../components/item_introduce.js'
+import React from "react";
 
 export default function Home() {
+  React.useEffect(() => {
+    import("lottie-interactive/dist/lottie-interactive.js");
+  });
   return (
     <div>
       <Sctracker />
@@ -13,6 +17,9 @@ export default function Home() {
       <Header />
       <Item_home />
       <Item_introduce />
+      {/* <div className="lottie_intro">
+          <lottie-interactive lottie-interactive path="/data/introduce_ani.json" interaction="hover"></lottie-interactive>
+      </div> */}
       <Footer />
     </div>
   )
