b7581098f481abc06225ed45fbd845b3e7208918
jjy331 <kingjjy31@gmail.com>
2023-06-06 01:11:30 +0900

    item_home escape scrolltrigger

--- pfjjy/components/item_home.js
+++ pfjjy/components/item_home.js
@@ -1,54 +1,108 @@
-import  React, {useRef, useEffect} from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import  React, {useState,useRef, useEffect} from "react";
+
 
 export default function Item_home() {
-    const  sectionRef = useRef(null);
-    const  plsdRef = useRef(null);
-    const triggerRef = useRef(null);
 
-    gsap.registerPlugin(ScrollTrigger);
+    const [scrollY, setScrollY] = useState(0);
+    const titleRef = useRef(null);
+    const sec1T_Ref = useRef(null);
+    const sec1RN_Ref = useRef(null);
+    const sec2T_Ref = useRef(null);
     
+    // Please, scroll down
+
     useEffect(() => {
 
+        const handleScroll = () => {
+            setScrollY(window.scrollY);
+        };
 
-        gsap.to(".pl_sd",{ opacity:0, duration:2, scrollTrigger: {trigger: ".pl_sd", start:"top 25%", end:"top 10%", scrub: true,}})
-
-        const sc1 = gsap.timeline({ scrollTrigger: { trigger:".sec1", start:"top 80%", end:"top 10%", scrub: true,}})
-
-        sc1.from(".runners",{ opacity:0, x:-300})
-        sc1.from(".sec1",{opacity:0,})
-        sc1.to(".sec1",{opacity:1,})
-        sc1.to(".runners",{ opacity:1, x:0})
-        sc1.to(".sec1",{opacity:0,})
-
-        const sc2 = gsap.timeline({scrollTrigger:{ trigger:".sec2", start: "top 80%", end:"top 10%", scrub: true,}})
-
-        sc2.from(".sec2",{opacity:0})
-        sc2.to(".sec2",{opacity:1})
-        sc2.to(".sec2",{opacity:0})
-
+        window.addEventListener('scroll', handleScroll);
 
         return () => {
-            // rnn.scrollTrigger.kill()
+            window.removeEventListener('scroll', handleScroll);
         }
-    },[])
+    },[]);
+
+    // sec1 animation
+
+    useEffect(() => {
+        const { current: titleElement } = titleRef;
+    
+        const pl_sd = Math.max(1 - scrollY / 1000, 0);
+
+        titleElement.style.display = scrollY <= 1000 ? 'block' : 'none';
+        titleElement.style.opacity = pl_sd.toString();
+
+    }, [scrollY]);
+
+    useEffect(() => {
+        const { current: sec1T_Element } = sec1T_Ref;
+    
+        if (scrollY >= 1000 && scrollY <= 3000) {
+            const opacity = Math.max((scrollY - 1000) / 2000, 0);
+            sec1T_Element.style.opacity = opacity.toString();
+            sec1T_Element.style.display = "block";
+        } else if (scrollY > 3000 && scrollY <= 5000) {
+            const opacity = Math.max(1 - (scrollY - 3000) / 2000, 0);
+            sec1T_Element.style.opacity = opacity.toString();
+            sec1T_Element.style.display = "block";
+        } else {
+            sec1T_Element.style.display = "none";
+        }
+    }, [scrollY]);
+    
+    useEffect(() => {
+        const { current: sec1RN_Element } = sec1RN_Ref;
+    
+        if (scrollY >= 1000 && scrollY <= 3000) {
+            const marginLeft = `${-200 + ((scrollY - 1000) / 2000) * 200}%`;
+            sec1RN_Element.style.marginLeft = marginLeft;
+            sec1RN_Element.style.display = "block";
+        } else if (scrollY > 3000 && scrollY <= 5000) {
+            const opacity = Math.max(1 - (scrollY - 3000) / 2000, 0);
+            sec1RN_Element.style.opacity = opacity.toString();
+            sec1RN_Element.style.display = "block";
+        } else {
+            sec1RN_Element.style.display = "none";
+        }
+    }, [scrollY]);
+
+
+    // sec2 animation 
+
+    useEffect(() => {
+        const { current: sec2T_Element } = sec2T_Ref;
+
+        if (scrollY >= 5000 && scrollY <= 7000) {
+            const opacity = Math.max((scrollY - 5000) / 2000, 0);
+            sec2T_Element.style.opacity = opacity.toString();
+            sec2T_Element.style.display = "block";
+        } else if (scrollY > 7000 && scrollY <= 9000) {
+            const opacity = Math.max(1 - (scrollY - 7000) / 2000, 0);
+            sec2T_Element.style.opacity = opacity.toString();
+            sec2T_Element.style.display = "block";
+        } else {
+            sec2T_Element.style.display = "none";
+        }
+
+    },[scrollY])
 
     return (
         <div>
-            <div className="sd_wrap">
-                <h2 className="pl_sd" ref={plsdRef}>Please, scroll down</h2>
+            <div  className="sd_wrap">
+                <h2 ref={titleRef} className="pl_sd">Please, scroll down</h2>
             </div>
-            <div className="sec1">
-                <img className="runners" src="/img/main.gif" alt="teamwork visual icon" />
-                <div className="typo1">
+            <div className="sec1" >
+                <img ref={sec1RN_Ref} className="runners" src="/img/main.gif" alt="teamwork visual icon" />
+                <div className="typo1" ref={sec1T_Ref}>
                     <h3 className="tw_1" >팀이 <span className="kw1">__</span>하면, 프로젝트가 <span className="kw2">__</span>된다</h3>
                     <p className="tw_2">일은 <span className="kw3">__</span> 하는 것이라 배웠습니다</p>
                 </div>
             </div>
             <div className="sec2">
                 <div className="sec2_wrap">
-                    <div className="typo2">
+                    <div className="typo2" ref={sec2T_Ref}>
                         <h3>팀이 단결하면, 프로젝트가 완결된다</h3>
                         <p>일은 같이 하는 것이라 배웠습니다</p>
                         <div className="d1"></div>
