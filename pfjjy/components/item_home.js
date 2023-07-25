8dca43f530e5ca238a582acb5a380874720218ec
jjy9331 <kingjjy31@gmail.com>
2023-07-25 19:08:04 +0900

    pf_home_runner_sc_ani_speed_control_update

--- pfjjy/components/item_home.js
+++ pfjjy/components/item_home.js
@@ -8,6 +8,11 @@ export default function Item_home() {
     const sec1T_Ref = useRef(null);
     const sec1RN_Ref = useRef(null);
     const sec2T_Ref = useRef(null);
+    const [currentFrame, setCurrentFrame] = useState(0);
+    const [imageFrames, setImageFrames1] = useState([]);
+    const [display, setDisplay] = useState("none");
+
+    const [imageLoaded, setImageLoaded1] = useState(Array(32).fill(true));
     
     // Please, scroll down
 
@@ -72,6 +77,52 @@ export default function Item_home() {
     }, [scrollY]);
 
 
+    useEffect(() => {
+        const startFrame = 1;
+        const endFrame = 32;
+        const startScrollY = 1000;
+        const endScrollY = 5000;
+        const speed = 4;
+    
+        // Calculate the progress with speed doubled
+        const progress = speed * (scrollY - startScrollY) / (endScrollY - startScrollY);
+    
+        // Calculate the looped progress from 0 to 1
+        const loopedProgress = progress % 1;
+    
+        // Calculate the frame using the looped progress value
+        const frame = Math.abs(Math.floor(startFrame + loopedProgress * (endFrame - startFrame)));
+    
+        setCurrentFrame(frame);
+        setDisplay("block");
+    
+    }, [scrollY]);
+
+    const handleImageLoad = (index) => {
+        setImageLoaded1((prevLoaded) => {
+            const loaded1 = [...prevLoaded];
+            loaded1[index] = true;
+            return loaded1;
+        });
+    };
+
+    // home_seq_webworker
+
+    useEffect(() => {
+        const loadImageFrames1 = async () => {
+            const frames = await Promise.all(
+                Array.from({ length: 32 }, async (_, index) => {
+                    const response = await fetch(`/team_runner_ani/${( index + 1 ).toString()}.svg`);
+                    const src = URL.createObjectURL(await response.blob());
+                    return { src, loaded: true };
+                })
+            );
+            setImageFrames1(frames);
+        };
+
+        loadImageFrames1();
+    }, []);
+
     // sec2 animation 
 
     useEffect(() => {
@@ -97,7 +148,22 @@ export default function Item_home() {
                 <h2 ref={titleRef} className="pl_sd">Please, scroll down</h2>
             </div>
             <div className="sec1" >
-                <img ref={sec1RN_Ref} className="runners" src="/img/main.gif" alt="teamwork visual icon" />
+                {/* <img ref={sec1RN_Ref} className="runners" src="/img/main.gif" alt="teamwork visual icon" /> */}
+                <div className="runners_wrap" ref={sec1RN_Ref}>
+                    {imageFrames.map((frame, index) => {
+                        if (!imageLoaded[index]) return null;
+                        return (
+                            <img
+                                key={index}
+                                className="runners"
+                                src={frame.src} 
+                                style={{ display: currentFrame === index ? "block" : "none" }}
+                                alt="runner_scroll_ani" 
+                                onLoad={() => handleImageLoad(index)}
+                            />
+                        );
+                    })}
+                </div>
                 <div className="typo1" ref={sec1T_Ref}>
                     <h3 className="tw_1" >팀이 <span className="kw1">__</span>하면, 프로젝트가 <span className="kw2">__</span>된다</h3>
                     <p className="tw_2">일은 <span className="kw3">__</span> 하는 것이라 배웠습니다</p>
