9bfe985eecc52d85e965b350a319fe262aa04b86
jjy9331 <jjy9331@naver.com>
2023-08-11 21:09:56 +0900

    header_1.2_h_btn_scY_update

--- pfjjy/components/header.js
+++ pfjjy/components/header.js
@@ -1,22 +1,30 @@
-
 import { useEffect } from 'react';
 
 export default function Header() {
     useEffect(() => {
-        const logoLink = document.querySelector('.logo');
+        const headerLinks = document.querySelectorAll('.h_btn');
 
-        const scrollToTop = () => {
-            window.scrollTo({
-                top: 0,
-                // behavior: 'smooth' // 부드러운 스크롤
-            });
+        const scrollToSection = (event) => {
+            const targetValue = event.currentTarget.getAttribute('data-value');
+
+            console.log("targetValue: "+targetValue);
+
+            if (targetValue) {
+                window.scrollTo({
+                    top: targetValue,
+                });
+            }
         };
 
-        if (logoLink) {
-            logoLink.addEventListener('click', scrollToTop);
+        if (headerLinks) {
+            headerLinks.forEach(link => {
+                link.addEventListener('click', scrollToSection);
+            });
 
             return () => {
-                logoLink.removeEventListener('click', scrollToTop);
+                headerLinks.forEach(link => {
+                    link.removeEventListener('click', scrollToSection);
+                });
             };
         }
     }, []);
@@ -26,38 +34,38 @@ export default function Header() {
             <div className="inner">
                 <div>
                     <a className="h_btn" data-value="0">
-                            <img src="/img/logo.png" alt="logo" className="logo h_btn" data-value="0" loading="lazy"/>
+                        <img src="/img/logo.png" alt="logo" className="logo h_btn" data-value="0" loading="lazy" />
                     </a>
                 </div>
                 <ul>
-                <li>
-                    <a  className="h_btn" data-value="0">홈</a>
-                    <div className="hr_d1"></div>
-                </li>
-                <li>
-                    <a  className="h_btn" data-value="14775">자기소개</a>
-                    <div className="hr_d2"></div>
-                    <div className="hr_d3"></div>
-                    <div className="hr_d4"></div>
-                    <div className="hr_d5"></div>
-                </li>
-                <li>
-                    <a  className="h_btn" data-value="63996">포트폴리오</a>
-                    <div className="hr_d6"></div>
-                    <div className="hr_d7"></div>
-                    <div className="hr_d8"></div>
-                    <div className="hr_d9"></div>
-                    <div className="hr_d10"></div>
-                </li>
-                <li>
-                    <a  className="h_btn" data-value="218740">컨텍트</a>
-                    <div className="hr_d11"></div>
-                    <div className="hr_d12"></div>
-                    <div className="hr_d13"></div>
-                    <div className="hr_d14"></div>
-                </li>
-            </ul>
+                    <li>
+                        <a className="h_btn" data-value="0">홈</a>
+                        <div className="hr_d1"></div>
+                    </li>
+                    <li>
+                        <a className="h_btn" data-value="11186">자기소개</a>
+                        <div className="hr_d2"></div>
+                        <div className="hr_d3"></div>
+                        <div className="hr_d4"></div>
+                        <div className="hr_d5"></div>
+                    </li>
+                    <li>
+                        <a className="h_btn" data-value="17899">포트폴리오</a>
+                        <div className="hr_d6"></div>
+                        <div className="hr_d7"></div>
+                        <div className="hr_d8"></div>
+                        <div className="hr_d9"></div>
+                        <div className="hr_d10"></div>
+                    </li>
+                    <li>
+                        <a className="h_btn" data-value="34736">컨텍트</a>
+                        <div className="hr_d11"></div>
+                        <div className="hr_d12"></div>
+                        <div className="hr_d13"></div>
+                        <div className="hr_d14"></div>
+                    </li>
+                </ul>
             </div>
         </div>
-    )
+    );
 }
