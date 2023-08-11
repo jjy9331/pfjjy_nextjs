0d0b8b5068bccec622c55eec4db7c66b70dd2288
jjy9331 <kingjjy31@gmail.com>
2023-08-11 18:42:53 +0900

    Next.js_imagloaded_1.0

---
+++ pfjjy/components/loading.js
@@ -0,0 +1,114 @@
+import React, { useEffect, useState } from 'react';
+
+const Loading = ({ onImagesLoaded }) => {
+  const [loadingProgress, setLoadingProgress] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadImageFrames = async (directory, numFrames, extension) => {
+      const frames = await Promise.all(
+        Array.from({ length: numFrames }, async (_, index) => {
+          const paddedIndex = index.toString().padStart(3, '0');
+          const response = await fetch(`/${directory}/${paddedIndex}.${extension}`);
+          if (!response.ok) {
+            throw new Error(`Failed to fetch image ${index}: ${response.status} ${response.statusText}`);
+          }
+          const src = URL.createObjectURL(await response.blob());
+          // console.log(`Loaded image frame ${index}: ${src}`);
+          return { src, loaded: false }; // 이미지 로드 상태를 false로 초기화
+        })
+      );
+      return frames;
+    };
+
+    const loadImages = async () => {
+      try {
+        const contactAniFrames = await loadImageFrames('contact_ani', 64, 'svg');
+        const introduceAniFrames = await loadImageFrames('introduce_ani', 153, 'png');
+        const imgPaths = [
+          '/img/hover_runner.gif',
+          '/img/hss_log.svg',
+          '/img/monitor.webp',
+          '/img/iphone.webp',
+          '/img/more_bg.svg',
+          '/img/pf2_eye.webp',
+          '/img/port1.png',
+          '/img/ypaint.webp',
+          // 다른 이미지 경로들도 여기에 추가
+        ];
+        const totalImages = contactAniFrames.length + introduceAniFrames.length + imgPaths.length;
+        let loadedImages = 0;
+
+        console.log("totalImages: "+ totalImages);
+
+        const loadImage = (url) =>
+          new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => {
+              // console.log('Loaded image:', url);
+              resolve();
+            };
+            img.onerror = () => {
+              // console.error('Failed to load image:', url);
+              reject();
+            };
+            img.src = url;
+          });
+
+        const updateProgress = () => {
+          loadedImages++;
+          console.log("loadedImages: "+ loadedImages);
+          const progress = Math.floor((loadedImages / totalImages) * 100);
+          
+          setLoadingProgress(progress);
+
+          console.log("progress: "+ progress);
+          if (loadedImages === totalImages) {
+            setLoadingProgress(100);
+            setImagesLoaded(true);
+          }
+        };
+
+        const allImagePromises = [
+          ...contactAniFrames.map((frame) => {
+            return loadImage(frame.src).then(() => {
+              frame.loaded = true; // 이미지 로드 상태를 업데이트
+              updateProgress();
+            });
+          }),
+          ...introduceAniFrames.map((frame) => {
+            return loadImage(frame.src).then(() => {
+              frame.loaded = true; // 이미지 로드 상태를 업데이트
+              updateProgress();
+            });
+          }),
+          ...imgPaths.map((url) => loadImage(url).then(updateProgress)),
+        ];
+
+        // 모든 이미지 로딩 프로미스 실행
+        await Promise.all(allImagePromises);
+        onImagesLoaded(); // 이미지 로딩 완료를 부모 컴포넌트에 알림
+        setImagesLoaded(true);
+      } catch (error) {
+        // console.error('Image loading error:', error);
+      }
+    };
+
+    if (!imagesLoaded) {
+      loadImages();
+    }
+  }, []); // 처음 로딩 시에만 실행
+
+  console.log('loadingProgress:', loadingProgress);
+
+  return (
+    <div className="loading-container">
+      <p className="loading-text">{loadingProgress}%</p>
+      <div className="progress">
+        <div className="progress-bar" style={{ width: `${loadingProgress}%` }} />
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
--- pfjjy/pages/index.js
+++ pfjjy/pages/index.js
@@ -1,3 +1,5 @@
+import React, { useEffect, useState } from 'react';
+
 import CustomCursor from '../components/cursor.js'
 import Header from '../components/header.js'
 import Footer from '../components/footer.js'
@@ -6,8 +8,32 @@ import Sctracker from '../components/sctracker.js'
 import Item_introduce from '../components/item_introduce.js'
 import Item_portfolio from '@/components/item_portfolio.js'
 import Item_contact from '@/components/item_contact.js'
+import Loading from '@/components/loading.js';
+
+
 
 export default function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  // useEffect(() => {
+  //   // Simulate some delay to show the loading page (optional)
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 1000); // Adjust the duration as needed
+  // }, []);
+
+  const handleImagesLoaded = () => {
+    setIsLoading(false); // 이미지 로딩이 완료되었으므로 로딩 상태 변경
+  };
+
+  // Show the loading page while loading is true
+  if (isLoading) {
+    console.log("image loaded3");
+    return <Loading onImagesLoaded={handleImagesLoaded} />;
+    
+  }
+
   return (
     <div>
       <Sctracker />
