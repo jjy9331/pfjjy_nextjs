266aabf8a468c62f6860d2c2107cef6491dcf61c
jjy9331 <jjy9331@naver.com>
2023-08-31 16:58:39 +0900

    port2_mousehover_ani_add

--- pfjjy/components/item_portfolio.js
+++ pfjjy/components/item_portfolio.js
@@ -106,12 +106,32 @@ export default function Item_portfolio() {
 
     },[scrollY])
 
+    // port 1 hover
+
     const handleMouseEnter = () => {
         setIsHovering(true);
     };
 
     const handleMouseLeave = () => {
         setIsHovering(false);
+    };
+
+    // port 2 hover
+
+    const handleMouseEnterPort2 = () => {
+        const port2Element = document.querySelector('.port_2');
+        const pf2EyeElement = port2Element.querySelector('.pf2_eye');
+        pf2EyeElement.style.bottom = '-11%';
+        pf2EyeElement.style.left = '-8%';
+        pf2EyeElement.style.transition = '0.3s ease-in-out 0.2s';
+    };
+
+    const handleMouseLeavePort2 = () => {
+        const port2Element = document.querySelector('.port_2');
+        const pf2EyeElement = port2Element.querySelector('.pf2_eye');
+        pf2EyeElement.style.bottom = '-54%';
+        pf2EyeElement.style.left = '-36%';
+        pf2EyeElement.style.transition = '0.3s ease-in-out';
     };
 
     return (
@@ -132,13 +152,22 @@ export default function Item_portfolio() {
                         />
                         </div>
                         <div className='port'>
-                            <img className='port_pc' src="/img/monitor.webp" alt="zerolab" />
+                            <img 
+                                className='port_pc' src="/img/monitor.webp" alt="zerolab" 
+                                onMouseEnter={handleMouseEnterPort2}
+                                onMouseLeave={handleMouseLeavePort2} 
+                            />
                             <div className='port_2'>
                                 <h3>zero<span>lab</span></h3>
                                 <p>리디자인</p>
                                 <div className="eye_hide1"></div>
                                 <div className="eye_hide2"></div>
-                                <img src="/img/pf2_eye.webp" alt="mouseover_ani_source" className="pf2_eye" loading="lazy" />
+                                <img 
+                                    src="/img/pf2_eye.webp" 
+                                    alt="mouseover_ani_source" 
+                                    className="pf2_eye" 
+                                    loading="lazy" 
+                                />
                             </div>
                         </div>
                         <div className='port'>
