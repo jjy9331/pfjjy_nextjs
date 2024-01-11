2484345057cf8e8da602dc80b75600fa8655018a
jjy9331 <jjy9331@naver.com>
2024-01-11 18:55:55 +0900

    port_more_hover_ani_update

--- pfjjy/components/item_portfolio.js
+++ pfjjy/components/item_portfolio.js
@@ -313,6 +313,26 @@ export default function Item_portfolio() {
         loadImageFrames_port5();
     }, []);
 
+
+
+
+    // port more hover ani
+
+    const handleMouseEnterPortm = () => {
+        const portmElement = document.querySelector('.port_more');
+        const pfmg = portmElement.querySelector('.hover_runner');
+        pfmg.style.zIndex =  -2;
+
+    };
+
+        const handleMouseLeavePortm = () => {
+        const portmElement = document.querySelector('.port_more');
+        const pfmg = portmElement.querySelector('.hover_runner');
+        pfmg.style.zIndex =  -4;
+    };
+
+
+
     return (
         <div>
             <div className='portfolio'>
@@ -453,12 +473,18 @@ export default function Item_portfolio() {
                         </div>
                         <div className='port' ref={portpmRef}>
                             <div className='pf_more'>
-                            <img className='port_ph' src="/img/iphone.webp" alt="port_more" />
-                            </div>
+                                <img 
+                                    className='port_ph' 
+                                    src="/img/iphone.webp" 
+                                    alt="port_more" 
+                                    onMouseEnter={handleMouseEnterPortm}
+                                    onMouseLeave={handleMouseLeavePortm} 
+                                />
                             <div className='port_more'>
                                 <img src="/img/more_bg.svg" className="pf_more_img" alt="pf_more" loading="lazy"/>
                                 <img src="/img/hover_runner.gif" className="hover_runner" alt="hover_runner" loading="lazy"/>
                                 <div className="pfmore_bg"></div>
+                            </div>
                             </div>
                         </div>
                     </div>
