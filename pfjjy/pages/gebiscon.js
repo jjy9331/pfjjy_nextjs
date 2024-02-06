c29510ceeaff903bb40080f6f1b2d34ce962205d
jjy9331 <jjy9331@naver.com>
2024-02-06 16:52:34 +0900

    pop_port_1_contents_layout_create

--- pfjjy/pages/gebiscon.js
+++ pfjjy/pages/gebiscon.js
@@ -25,12 +25,31 @@ export default function Gebiscon({onClose}) {
     return (
         <div id='port_pop' className='port_pop' ref={modalRef} >
             <Pop_h title="UI UX mindset" onClose={onClose} onPpTClick={handlePpTClick} />
-            <div className='pop_test'>test1</div>
-            <div className='pop_test'>test2</div>
-            <div className='pop_test'>test3</div>
-            <div className='pop_test'>test4</div>
-            <div className='pop_test'>test5</div>
-            <div className='pop_test'>test6</div>
+            <div className='sc_al'>
+                <img className="sc_w" src="/img/port_1/screen_size_control.svg" alt="interactive_screen_alert" loading="lazy"/>
+            </div>
+            <div className='sheet s1'>
+                <div className="s1_left"></div>
+                <div className="s1_right"></div>
+                <img className='s1_img' src="/img/port_1/sec1.webp" alt="sec1" />
+            </div>
+            <div className='sheet s2'>
+                <img src="/img/port_1/sec2.webp" alt="sec2" />
+            </div>
+            <div className='sheet s3'>
+                <img src="/img/port_1/sec3.webp" alt="sec3" />
+            </div>
+            <div className='sheet s4'>
+                <img src="/img/port_1/sec4.webp" alt="sec4" />
+            </div>
+            <div className='sheet s5'>
+                <img src="/img/port_1/sec5.webp" alt="sec5" />
+            </div>
+            <div className='sheet s6'>
+                <div className="s6_left"></div>
+                <div className="s6_right"></div>
+                <img className='s6_img' src="/img/port_1/sec6.gif" alt="sec6" />
+            </div>
             <Pop_f/>
         </div>
     )
